da3f699a19836205ade6f51f40a20f640c44ff02
hampusfurhoffeniro <hampus.furhoff@eniro.com>
2021-04-14 17:45:41 +0200

    set up static code analysis

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ function createServer(config = {}) {
   const server = http.createServer((req, res) => {
     console.log(`Received request ${req.method.toUpperCase()} ${req.url}`);
     res.writeHead(200, "OK");
+    console.log("hej");
+    console.log("då");
     res.write(Buffer.from(`Hello World! Build number ${buildNumber}\n`));
     res.end();
   });
