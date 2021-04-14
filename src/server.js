11dcff2a02752f01011aa9574f8bab3fc4bd5441
hfurhoff <hampus.furhoff@eniro.com>
2021-04-14 18:32:36 +0200

    removed bad code and added prettier command

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ function createServer(config = {}) {
 
   const server = http.createServer((req, res) => {
     console.log(`Received request ${req.method.toUpperCase()} ${req.url}`);
-    res.writeHead(200, "OK");console.log("hej");console.log("då");
+    res.writeHead(200, "OK");
     res.write(Buffer.from(`Hello World! Build number ${buildNumber}\n`));
     res.end();
   });
